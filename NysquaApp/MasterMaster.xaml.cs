b7faecfa7b3b625fcefb89aac2adbda1b289f1ad
wdcardenasb <38057700+wdcardenasb@users.noreply.github.com>
2018-05-05 09:56:55 -0500

    Initial commit

---
+++ NysquaApp/MasterMaster.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NysquaApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public MasterMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new MasterMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MasterMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MasterMenuItem> MenuItems { get; set; }
+            
+            public MasterMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MasterMenuItem>(new[]
+                {
+                    new MasterMenuItem { Id = 0, Title = "Page 1" },
+                    new MasterMenuItem { Id = 1, Title = "Page 2" },
+                    new MasterMenuItem { Id = 2, Title = "Page 3" },
+                    new MasterMenuItem { Id = 3, Title = "Page 4" },
+                    new MasterMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
