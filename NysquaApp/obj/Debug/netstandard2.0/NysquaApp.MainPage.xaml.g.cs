498752e7608af36f1331fe846dd249b02c99d39c
wdcardenasb <38057700+wdcardenasb@users.noreply.github.com>
2018-05-05 11:10:52 -0500

    Layout

--- NysquaApp/obj/Debug/netstandard2.0/NysquaApp.MainPage.xaml.g.cs
+++ NysquaApp/obj/Debug/netstandard2.0/NysquaApp.MainPage.xaml.g.cs
@@ -19,12 +19,16 @@ namespace NysquaApp {
         private global::Xamarin.Forms.Image Img1;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Image Logo;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private global::Xamarin.Forms.Label reserv;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
             Img1 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Image>(this, "Img1");
+            Logo = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Image>(this, "Logo");
             reserv = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "reserv");
         }
     }
